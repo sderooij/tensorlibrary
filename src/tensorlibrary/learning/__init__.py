908f14670b40251d235352ded06c23461dd2019f
Seline de Rooij <67693178+sderooij@users.noreply.github.com>
2025-04-25 14:21:15 +0200

    delete code for release

--- src/tensorlibrary/learning/__init__.py
+++ src/tensorlibrary/learning/__init__.py
@@ -1,3 +1,3 @@
 from .features import features
 from .tt_krr import update_wz_tt, get_tt_rank, get_g, initialize_wz
-from .t_krr import CPKRR, TTKRR, CPKRR_Adapt, CPKRR_LMPROJ
+from .t_krr import CPKRR, TTKRR, CPKRR_Adapt
